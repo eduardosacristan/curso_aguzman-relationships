90cf3c764176d5665437f283a5c0c26e9803519b
eduardosacristan <eduardosacristan@outlook.es>
2025-09-23 17:19:48 +0100

    entities created

--- src/main/java/com/andres/cursospringboot/jpa/springboot_jpa_relationship/entities/Invoice.java
+++ src/main/java/com/andres/cursospringboot/jpa/springboot_jpa_relationship/entities/Invoice.java
@@ -13,6 +13,35 @@ public class Invoice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
 
+    private String description;
+    private Long total;
+    
+    public Invoice(Long id, String description, Long total) {
+        this.id = id;
+        this.description = description;
+        this.total = total;
+    }
+    public Invoice() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Long getTotal() {
+        return total;
+    }
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    
 }
