add6b3b43b36a414192347e81363756a1b2320cc
eduardosacristan <eduardosacristan@outlook.es>
2025-09-23 17:16:30 +0100

    first commit

---
+++ src/main/java/com/andres/cursospringboot/jpa/springboot_jpa_relationship/entities/Client.java
@@ -0,0 +1,17 @@
+package com.andres.cursospringboot.jpa.springboot_jpa_relationship.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
